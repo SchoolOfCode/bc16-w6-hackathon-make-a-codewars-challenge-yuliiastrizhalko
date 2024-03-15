934244925c7c757b29a103b1b5cbd2edc54edc83
yuliiastrizhalko <321gadzila12366@gmail.com>
2024-03-15 14:02:08 +0200

    linespace

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ export function countLeapYears(startYear, endYear) {}
 
 // our code has just two parameters - startYear and endYear
 // count leap years from the first year of our era to 2024 A.D. inclusive ➕
+
 // if you would like extra exercises you can try next 👇
 // Count leap years from 2000 to 2024
 // Count leap years from 100 to 200
