3113ab2b2fcd300f7d89b7b2f9ef543b1c3b06f6
yuliiastrizhalko <321gadzila12366@gmail.com>
2024-03-15 14:01:14 +0200

    Extra test

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
- export function countLeapYears(startYear, endYear) {}
+export function countLeapYears(startYear, endYear) {}
 
 // our code has just two parameters - startYear and endYear
 // count leap years from the first year of our era to 2024 A.D. inclusive ➕
+// if you would like extra exercises you can try next 👇
+// Count leap years from 2000 to 2024
+// Count leap years from 100 to 200
+// Count leap years from 2020 to 2024
+// Good luck 🍀
--- main.test.js
+++ main.test.js
@@ -6,3 +6,15 @@ test("Count leap years between 1 and 2024", () => {
   const actual = countLeapYears(1, 2024);
   expect(actual).toBe(expected);
 });
+
+test("Count leap years from 2000 to 2024", () => {
+  expect(countLeapYears(2000, 2024)).toBe(6); // 2000, 2004, 2008, 2012, 2016, 2020
+});
+
+test("Count leap years from 100 to 200", () => {
+  expect(countLeapYears(100, 200)).toBe(24); // Every 4th year starting from 100 is a leap year
+});
+
+test("Count leap years from 2020 to 2024", () => {
+  expect(countLeapYears(2020, 2024)).toBe(2); // 2020, 2024
+});
